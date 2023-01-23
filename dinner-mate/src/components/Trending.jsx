04d79e2d600051f217124b959bc56cdcbaa6cce2
Divij Seth <divijseth@Divijs-MBP.hub>
2023-01-23 17:51:20 +1300

    trending complete

--- dinner-mate/src/components/Trending.jsx
+++ dinner-mate/src/components/Trending.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
 import { useEffect } from "react"
 import styled from "styled-components"
-
-
+import { Splide, SplideSlide} from "@splidejs/react-splide"
+import "@splidejs/splide/dist/css/splide.min.css"
 
 
 const Trending = () => {
@@ -21,21 +21,27 @@ const Trending = () => {
     }
     return (
         <>
-            {trending.map((recipe) => {
-                return (
-                    <Wrapper>
-                        <h3>Trending</h3>
-                        {trending.map((recipe) => {
-                            return (
-                                <Card>
-                                    <p>{recipe.title}</p>
-                                    <img src={recipe.image} alt={recipe.title} />
-                                </Card>                                
-                            )
-                        })}
-                    </Wrapper>
-                )
-            })}
+            <Wrapper>
+                <h3>Trending</h3>
+                <Splide options={{
+                    perPage: 4,
+                    arrows: true,
+                    pagination: false,
+                    gap: "5rem",
+                }}>
+                {trending.map((recipe) => {
+                    return (
+                        <SplideSlide>
+                            <Card>
+                                <p>{recipe.title}</p>
+                                <img src={recipe.image} alt={recipe.title} />
+                                <Gradient />
+                            </Card>
+                        </SplideSlide>                              
+                    )
+                })}                            
+                </Splide>
+            </Wrapper>
         </>
     )
 }
@@ -47,6 +53,43 @@ const Wrapper = styled.div`
 const Card = styled.div`
     min-height: 25rem;
     border-radius: 2rem;
+    overflow: hidden;
+    position: relative;
+
+    img {
+        border-radius: 2rem;
+        postion: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        
+    }
+
+    p {
+        position: absolute;
+        z-index: 10;
+        left: 50%;
+        bottom: 0%;
+        transform: translate(-50%, 0%);
+        color: white;
+        width: 100%;
+        text-align: center;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;
+        justify-content: center;
+
+    }
 `;
+
+const Gradient = styled.div`
+    z-index: 3;
+    postion: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5)
+`
 
 export default Trending
