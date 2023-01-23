9b86a7c7364ac98711840eb797204e0d15f1f2d8
Divij Seth <divijseth@Divijs-MBP.hub>
2023-01-23 20:17:57 +1300

    completed barebones of icons in category

--- dinner-mate/src/components/Category.jsx
+++ dinner-mate/src/components/Category.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { MdOutlineFreeBreakfast } 
+import { MdOutlineFreeBreal, MdOutlineLunchDin } from "react-icons/md"
+import { GiChopsticks } from "react-icons/gi"
+import { BiCake } from "react-icons/bi"
 
 const Category = () => {
     return (
         <List>
             <Navlink>
-                <Fa
+                <MdOutlineFreeBreal />
+                <h4>Breakfast</h4>
+            </Navlink>
+            <Navlink>
+                <MdOutlineLunchDin />
+                <h4>Lunch</h4>
+            </Navlink> 
+            <Navlink>
+                <GiChopsticks />
+                <h4>Dinner</h4>
+            </Navlink> 
+            <Navlink>
+                <BiCake />
+                <h4>Desert</h4>
             </Navlink> 
         </List>
     )
