310c61fb419f5dd4e0aa4f6cc7f3cdb8b7464c5d
Divij Seth <divijseth@Divijs-MBP.hub>
2023-01-24 17:42:49 +1300

    completed barebones category and styling of icons/buttons, routes still need to be added

--- dinner-mate/src/components/Category.jsx
+++ dinner-mate/src/components/Category.jsx
@@ -7,24 +7,66 @@ import { Navlink } from "react-router-dom"
 const Category = () => {
     return (
         <List>
-            <Navlink>
+            <Slink>
                 <AiOutlineCoffee />
                 <h4>Breakfast</h4>
-            </Navlink>
-            <Navlink>
+            </Slink>
+            <Slink>
                 <MdOutlineLunchDin />
                 <h4>Lunch</h4>
-            </Navlink> 
-            <Navlink>
+            </Slink> 
+            <Slink>
                 <GiChopsticks />
                 <h4>Dinner</h4>
-            </Navlink> 
-            <Navlink>
+            </Slink> 
+            <Slink>
                 <BiCake />
                 <h4>Desert</h4>
-            </Navlink> 
+            </Slink> 
         </List>
     )
 }
+
+const List = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 2rem 0rem;
+`;
+
+const Slink = styled(Navlink)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 2rem;
+    text-decoration: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    width: 6rem;
+    height: 6rem;
+    cursor: pointer;
+    transform: scale(0.8);
+
+    h4 {
+        color: white;
+        font-size: 8rem;
+
+    }
+    svg {
+        color: white;
+        font-size: 1.5rem;
+    }
+    &.active {
+        background: linear-gradient(to right, #f27121, #e94057);
+
+        svg {
+            color: white;
+        }
+        h4 {
+            color: white;
+        }
+    }
+`;
+
 
 export default Category
