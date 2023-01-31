29c638672788b190dd5ae63738b01ef3baa10b6c
Divij Seth <divijseth@Divijs-MBP.hub>
2023-01-31 20:13:13 +1300

    navbar logo added

--- dinner-mate/src/components/Navbar.jsx
+++ dinner-mate/src/components/Navbar.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { NavLogo } from './logo.svg'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <Nav>
+            <Logo to={'/'}/>
+            <NavLogo />
+        </Nav>
+    )
 }
 
-const Logo = styled(Link)
+const Logo = styled(Link)`
+    text-decoration: none;
+    font-size: 1.5rem;
+
+`;
+
+const Nav = styled.div`
+    padding: 4rem 0rem;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    svg {
+        font-size: 2rem;
+        fill: #085659;
+
+    }
+`;
+
 export default Navbar
