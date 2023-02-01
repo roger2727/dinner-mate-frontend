0cac0220c441588946d5560f8d85fca3a5a667aa
Divij Seth <divijseth@Divijs-MBP.hub>
2023-02-02 12:27:20 +1300

    added more styling to login, need to attach the same to sign up

--- dinner-mate/src/components/Login.jsx
+++ dinner-mate/src/components/Login.jsx
@@ -11,12 +11,12 @@ const Login = () => {
     }
 
     return (
-        <MainContainer>
+        <MainContainer onSubmit={submit}>
             <LoginText>
                 Log In
             </LoginText>
             <InputContainer>
-                <form onSubmit={submit}>
+                {/* <form onSubmit={submit}> */}
                     <StyledInput
                         type= "text"
                         placeholder="Username"
@@ -29,18 +29,24 @@ const Login = () => {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                <button type="submit">Login</button>
-                </form>
+                {/* </form> */}
             </InputContainer>
+            <ButtonContainer>
+                <StyledLogin type="submit">Login</StyledLogin>    
+            </ButtonContainer>
+            <SignUp>Don't have an account? Sign Up here!</SignUp>
         </MainContainer>
 
     )
 }
 
-const MainContainer = styled.div`
+const MainContainer = styled.form`
     display: flex;
+    left-margin: auto;
+    right-margin: auto;
     align-items: center;
     flex-direction: column;
+    justify-content center;
     height: 80vh;
     width: 30vw;
     background: linear-gradient(35deg, #494949, #313131);
@@ -71,5 +77,30 @@ const StyledInput = styled.input`
     border: none;
     outline: none;
 `;
+
+const ButtonContainer = styled.div`
+    margin: 1rem 0 2rem 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`;
+
+const StyledLogin = styled.button`
+    background:  background: linear-gradient(to right, #f27121, #e94057);
+    text-transform: uppercase;
+    letter-spacing: 0.2rem;
+    width: 65%;
+    height: 3rem;
+    color: white;
+    cursor: pointer;
+
+
+`;
+
+const SignUp = styled.h4`
+    cursor: pointer;
+`
 
 export default Login
