f3b9bb96718f7b6ffa3bcf7619021c966e9c0ba5
Mitchell Roger <roger.mitch27@gmail.com>
2023-02-04 17:58:11 +1100

    fixed

--- dinner-mate/src/components/EditRecipe.jsx
+++ dinner-mate/src/components/EditRecipe.jsx
@@ -51,61 +51,74 @@ const EditRecipe = () => {
   };
 
   return (
-    <form onSubmit={handleUpdate}>
-      <h1>You want to make changes? We get it, update away...</h1>
-      <Category />
-      <input
-        type="text"
-        name="title"
-        placeholder="Name your Connection here..."
-        value={recipe.title}
-        onChange={(e) => setRecipe({ ...recipe, title: e.target.value })}
-      />
-      <textarea
-        name="ingredients"
-        placeholder="Example: 
-            Salt
-            Chicken
-            Salad"
-        value={recipe.ingredients.join("\n")}
-        onChange={(e) =>
-          setRecipe({ ...recipe, ingredients: e.target.value.split("\n") })
-        }
-        required
-      />
-      <textarea
-        name="Instructions"
-        placeholder="Example: 
-            Preheat oven to 350°F.
-            In a large bowl, combine flour, sugar, and baking powder.
-            Add in the butter, eggs, and milk. Mix until well combined."
-        value={recipe.instructions}
-        onChange={(e) => setRecipe({ ...recipe, instructions: e.target.value })}
-        required
-      />
-      <select
-        name="Serving Size"
-        value={recipe.servingSize}
-        onChange={(e) => setRecipe({ ...recipe, servingSize: e.target.value })}
-        required
-      >
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-      </select>
-      <input
-        type="text"
-        name="cookingTime"
-        placeholder="Cooking Time (in minutes)"
-        value={recipe.cookingTime}
-        onChange={(e) => setRecipe({ ...recipe, cookingTime: e.target.value })}
-      />
-      <button type="submit">Update Recipe</button>
+    <form className="update-form">
+      <label>
+        Title:
+        <input
+          type="text"
+          value={recipe.title}
+          onChange={(event) =>
+            setRecipe({ ...recipe, title: event.target.value })
+          }
+        />
+      </label>
+      <br />
+      <label>
+        Ingredients:
+        <textarea
+          value={recipe.ingredients}
+          onChange={(event) =>
+            setRecipe({ ...recipe, ingredients: event.target.value })
+          }
+        />
+      </label>
+      <br />
+      <label>
+        Instructions:
+        <textarea
+          value={recipe.instructions}
+          onChange={(event) =>
+            setRecipe({ ...recipe, instructions: event.target.value })
+          }
+        />
+      </label>
+      <br />
+      <label>
+        Category:
+        <input
+          type="text"
+          value={recipe.category}
+          onChange={(event) =>
+            setRecipe({ ...recipe, category: event.target.value })
+          }
+        />
+      </label>
+      <br />
+      <label>
+        Cooking Time:
+        <input
+          type="number"
+          value={recipe.cookingTime}
+          onChange={(event) =>
+            setRecipe({ ...recipe, cookingTime: event.target.value })
+          }
+        />
+      </label>
+      <br />
+      <label>
+        Serving Size:
+        <input
+          type="number"
+          value={recipe.servingSize}
+          onChange={(event) =>
+            setRecipe({ ...recipe, servingSize: event.target.value })
+          }
+        />
+      </label>
+      <br />
+      <button type="submit" onClick={handleUpdate}>
+        Update Recipe
+      </button>
     </form>
   );
 };
