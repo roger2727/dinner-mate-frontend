2c2cafa50069da47f8be96c78c5e83e15e62b2a4
Dale Dahlenburg <dale.19@icloud.com>
2023-02-05 00:01:59 +1000

    fixed width

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -72,26 +72,6 @@ const Recipe = () => {
   );
 };
 
-const DetailWrapper = styled.div`
-  margin-top: 10rem;
-  margin-bottom: 5rem;
-  display: flex;
-  h2 {
-    margin-bottom: 2rem;
-  }
-  .active {
-    background: linear-gradient(35deg, #494949, #313131);
-    color: white;
-  }
-  li {
-    font-size: 1.2rem;
-    line-height: 2.5rem;
-  }
-  ul {
-    margin-top: 2rem;
-  }
-`;
-
 const Button = styled.button`
   padding: 1rem 2rem;
   color: #313131;
