b9129f5e019775b2394e6b6f7a80c51feef731a5
Dale Dahlenburg <dale.19@icloud.com>
2023-02-04 21:52:30 +1000

    fix

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -21,7 +21,7 @@ const Pages = () => {
         <Route path="/recipe/:id" element={<><Navbar /><Recipe /></>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/addrecipe" element={<><Navbar /><AddRecipe /><AddImage /></>} />
+        <Route path="/addrecipe" element={<><Navbar /><AddRecipe /></>} />
         <Route path="/myrecipes" element={<MyRecipes />} />
         <Route path="/editrecipe/:recipeId" element={<><Navbar /><EditRecipe /></>} />
         <Route path="add-image/:recipeId" element={<><Navbar /><AddImage /></>} />
