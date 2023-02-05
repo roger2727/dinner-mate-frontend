3d8972506b7425b6022fa693e814df5efea7214c
Divij Seth <divijseth@Divijs-MBP.hub>
2023-02-06 00:39:10 +1300

    load in animation only on first time

--- src/components/TodayPicks.jsx
+++ src/components/TodayPicks.jsx
@@ -93,10 +93,10 @@ const Card = styled.div`
 
 const Gradient = styled.div`
   z-index: 3;
-  postion: absolute;
+  position: absolute;
   width: 100%;
   height: 100%;
-  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
 `;
 
 export default TodayPicks;
