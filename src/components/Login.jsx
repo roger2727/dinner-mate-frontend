b0d6c8a1590e993ab49eab7bd4cf7acb8379afea
Mitchell Roger <roger.mitch27@gmail.com>
2023-02-05 19:15:31 +1100

    fixed form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -107,47 +107,129 @@ const Login = () => {
 };
 
 const MainContainer = styled.form`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   display: flex;
-  flex-direction: column;
+  left-margin: auto;
+  right-margin: auto;
   align-items: center;
-  padding: 10px; ;
+  flex-direction: column;
+  justify-content center;
+  height: 80vh;
+  width: 30vw;
+  background: linear-gradient(35deg, #494949, #313131);
+  border-radius: 10px;
+  color: #ffffff;
+
+
+  @media only screen and (max-width: 320px) {
+    width: 80vw;
+    height: 90vh;
+    hr {
+      margin-bottom: 0.3rem;
+    }
+    h4 {
+      font-size: small;
+    }
+  }
+  @media only screen and (min-width: 360px) {
+    width: 80vw;
+    height: 90vh;
+    h4 {
+      font-size: small;
+    }
+  }
+  @media only screen and (min-width: 411px) {
+    width: 80vw;
+    height: 90vh;
+  }
+  @media only screen and (min-width: 768px) {
+    width: 80vw;
+    height: 80vh;
+  }
+  @media only screen and (min-width: 1024px) {
+    width: 70vw;
+    height: 50vh;
+  }
+  @media only screen and (min-width: 1280px) {
+    width: 30vw;
+    height: 80vh;
+  }
 `;
-const LoginText = styled.label`
-  font-size: 36px;
-  font-weight: bold;
-  margin-bottom: 10px; ;
+
+const LoginText = styled.h3`
+  margin: 3rem 0 2rem 0;
+  color: white;
 `;
 
 const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center; ;
+  justify-content: space-around;
+  align-items: center;
+  height: 20%;
+  width: 100%;
 `;
 
 const StyledInput = styled.input`
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
-  font-size: 20px; ;
-`;
-
-const ErrorMessage = styled.div`
-  color: red;
-  margin-bottom: 10px; ;
-`;
-const Button = styled.button`
-  width: 100%;
-  padding: 10px;
-  background-color: #ffc107;
-  color: white;
-  font-size: 20px;
+  background: rgba(255, 255, 255, 0.15);
+  box-shadow: 0 8px 32 px 0 rgba(31, 38, 135, 0.375);
+  width: 80%;
+  border-radius: 1rem;
+  height: 3rem;
+  padding: 1rem;
   border: none;
-  cursor: pointer; ;
+  outline: none;
+  &:focus {
+    display: inline-block;
+    box-shadow: 0 0rem 0 0.1rem #f27121;
+    backdrop-filter: blur(12rem);
+    border-radius: 1rem;
+  }
+  &::placeholder {
+    color: white;
+  }
 `;
 
-const StyledLink = styled(Link)`
-  color: blue;
-  margin-top: 10px;
-  font-size: 20px; ;
+const ButtonContainer = styled.div`
+  margin: 1rem 0 2rem 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledLogin = styled.button`
+  background: #f27121;
+  text-transform: uppercase;
+  letter-spacing: 0.2rem;
+  width: 50%;
+  height: 3rem;
+  color: white;
+  cursor: pointer;
+  border-radius: 2rem;
+  border: none;
+`;
+
+const Slink = styled(Link)`
+  text-decoration: none;
+  color: white;
+`;
+const SignUp = styled.h4`
+  cursor: pointer;
+  color: white;
+`;
+
+const HorizontalRule = styled.hr`
+  width: 90%;
+  height: 0.3rem;
+  border-radius: 0.8rem;
+  border: none;
+  background: white;
+  background-color: #ebd0d0;
+  margin: 1.5rem 0 1rem 0;
+  backdrop-filter: blur(25px);
 `;
 export default Login;
